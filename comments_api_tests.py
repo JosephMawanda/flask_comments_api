04df75731ca86d3634c4c66ffd90680b73dc9a13
Joseph Balikuddembe <33752813+JosephMawanda@users.noreply.github.com>
2018-12-28 21:17:49 +0300

    Create comments_api_tests.py

---
+++ comments_api_tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+user1 = user('User 1')
+mod = moderator('Moderator')
+
+class Test(unittest.TestCase):
+  def test_instantiation(self):
+    self.assertEqual(user1.name,'User 1', 'User name is set correctly')
+    user1.name = 'User 1 Updated'
+    self.assertEqual(user1.name,'User 1 Updated', 'User name can be updated')
+    self.assertIsInstance(mod, user, 'Moderator is a user')
